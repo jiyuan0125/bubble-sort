40044e9625d228e47bd31e3300b1f69283b10fb4
jiyuan0125 <jiyuan0125@126.com>
2022-12-02 16:27:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bubble Sort
+
+Bubble sort function written in rust.
+
+## Screenshot
+
+![Result](./screenshot.png)
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn bubble_sort<T: PartialOrd>(arr: &mut [T]) {
+    for i in 0..arr.len() - 1 {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut arr = [5, 4, 3, 2, 1];
+    println!("original i32 array: {:?}", arr);
+    bubble_sort(&mut arr);
+    println!("sorted i32 array: {:?}", arr);
+    assert_eq!(arr, [1, 2, 3, 4, 5]);
+
+    let mut arr = ["a01", "01a", "1a0", "0a1", "10a", "a10"];
+    println!("original str array: {:?}", arr);
+    bubble_sort(&mut arr);
+    println!("sorted str array: {:?}", arr);
+    assert_eq!(arr, ["01a", "0a1", "10a", "1a0", "a01", "a10"]);
+}
